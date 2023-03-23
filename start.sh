719f1e4ea38478da877e850dcb1681177fb5e740
rajam0723 <99820637+rajam0723@users.noreply.github.com>
2023-03-23 16:26:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/lazyindu/Lazyrrr.git /LazyPrincess
+  git clone https://github.com/rajam0723/2023-rp 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LazyPrincess
