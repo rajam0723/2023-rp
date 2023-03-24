d54548a5fcdfc24d7907a8c2ce47942ed9db0c6b
rajam0723 <99820637+rajam0723@users.noreply.github.com>
2023-03-24 15:58:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,23 @@
 if [ -z $UPSTREAM_REPO ]
+
 then
+
   echo "Cloning main Repository"
-  git clone https://github.com/rajam0723/2023-rp.git /2023-rp
+
+  gh repo clone rajam0723/2023-rp
+
 else
+
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
+
   git clone $UPSTREAM_REPO /2023-rp
+
 fi
+
 cd /2023-rp
+
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+
+echo "Starting Bot..."
+
 python3 bot.py
