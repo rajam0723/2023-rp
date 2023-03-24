e139664717112982d862350d3faef156b9cefe43
rajam0723 <99820637+rajam0723@users.noreply.github.com>
2023-03-24 15:08:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/rajam0723/2023-rp.git /2023-rp
+  git clone https://github.com/rajam0723/2023-rp.git /Dudu
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /2023-rp
+  git clone $UPSTREAM_REPO /Dudu
 fi
 cd /2023-rp
 pip3 install -U -r requirements.txt
